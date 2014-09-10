d45a2d4698bd88a6e42d31c596e2d80d242174e8
rwtema <rwtema@gmail.com>
2014-09-11 00:41:53 +0100

    Started work on FMP Frame

---
+++ src/main/java/com/rwtema/funkylocomotion/fmp/FramePart.java
@@ -0,0 +1,37 @@
+package com.rwtema.funkylocomotion.fmp;
+
+import codechicken.lib.vec.Cuboid6;
+import codechicken.multipart.TMultiPart;
+import codechicken.multipart.TSlottedPart;
+import codechicken.multipart.minecraft.McMetaPart;
+import com.rwtema.funkylocomotion.FunkyLocomotion;
+import net.minecraft.block.Block;
+
+public class FramePart extends McMetaPart implements TSlottedPart {
+    private int slotMask;
+    {
+        slotMask = 0;
+        for (int i = 15; i <= 26; i++)
+            slotMask = slotMask | (1 << i);
+    }
+
+    @Override
+    public String getType() {
+        return FunkyLocomotion.MODID + ":frame";
+    }
+
+    @Override
+    public int getSlotMask() {
+        return slotMask;
+    }
+
+    @Override
+    public Block getBlock() {
+        return null;
+    }
+
+    @Override
+    public Cuboid6 getBounds() {
+        return null;
+    }
+}
