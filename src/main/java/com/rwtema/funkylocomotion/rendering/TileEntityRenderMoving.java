e1e251a55501d11567034c34773d78f305c3aea9
rwtema <rwtema@gmail.com>
2014-09-27 07:47:07 +0100

    Split out methods for rendering static and dynamic components.

--- src/main/java/com/rwtema/funkylocomotion/rendering/TileEntityRenderMoving.java
+++ src/main/java/com/rwtema/funkylocomotion/rendering/TileEntityRenderMoving.java
@@ -52,83 +52,14 @@ public class TileEntityRenderMoving extends TileEntitySpecialRenderer {
             GL11.glTranslated(-mover.xCoord, -mover.yCoord, -mover.zCoord);
             GL11.glTranslated(dir.offsetX * h, dir.offsetY * h, dir.offsetZ * h);
 
-            RenderHelper.disableStandardItemLighting();
-            
-            if (pass != 0) {
-                GL11.glEnable(GL11.GL_BLEND);
-                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            }
-
-            GL11.glEnable(GL11.GL_CULL_FACE);
-
-            if (Minecraft.isAmbientOcclusionEnabled()) {
-                GL11.glShadeModel(GL11.GL_SMOOTH);
-            } else {
-                GL11.glShadeModel(GL11.GL_FLAT);
-            }
-
-            try {
-                tessellator.setColorOpaque_F(1.0F, 1.0F, 1.0F);
-                tessellator.startDrawingQuads();
-
-                if (renderBlocks.renderBlockByRenderType(mover.block, mover.xCoord, mover.yCoord, mover.zCoord))
-                    flag = true;
-
-                tessellator.draw();
-            } catch (Exception e) {
-                FLRenderHelper.clearTessellator();
-
-                (new RuntimeException(
-                        "Unable to render block " + Block.blockRegistry.getNameForObject(mover.block)
-                                + " with meta " + mover.meta + " at ("
-                                + mover.xCoord + "," + mover.yCoord + mover.zCoord + "). Disabling Rendering."
-                        , e
-                )).printStackTrace();
-
-                TileMovingClient.renderErrorList.add(mover.block.getClass());
-                mover.tile = null;
-                mover.render = false;
-                mover.error = true;
-                return;
-            }
-            RenderHelper.enableStandardItemLighting();
+            flag = renderStatic(mover, pass);
 
             GL11.glPopMatrix();
             GL11.glEnable(GL11.GL_CULL_FACE);
 
         }
 
-        if (mover.tile != null && mover.tile.shouldRenderInPass(pass)) {
-            TileEntitySpecialRenderer specialRenderer = TileEntityRendererDispatcher.instance.getSpecialRenderer(mover.tile);
-            if (specialRenderer != null) {
-                GL11.glPushMatrix();
-                GL11.glTranslated(dir.offsetX * h, dir.offsetY * h, dir.offsetZ * h);
-                try {
-                    specialRenderer.func_147496_a(fakeWorld);
-                    specialRenderer.renderTileEntityAt(mover.tile, x, y, z, f);
-                    specialRenderer.func_147496_a(world);
-                } catch (Exception e) {
-                    FLRenderHelper.clearTessellator();
-
-                    (new RuntimeException(
-                            "Unable to render TSER " + mover.tile.getClass().getName() + " for "
-                                    + Block.blockRegistry.getNameForObject(mover.block)
-                                    + " with meta " + mover.meta + " at ("
-                                    + mover.xCoord + "," + mover.yCoord + mover.zCoord + "). Disabling Rendering."
-                            , e
-                    )).printStackTrace();
-
-                    TileMovingClient.renderErrorList.add(mover.tile.getClass());
-                    mover.error = true;
-                    mover.tile = null;
-                    mover.render = false;
-                    return;
-                }
-
-                flag = true;
-                GL11.glPopMatrix();
-            }
-        }
+        flag = flag | renderDynamic(x, y, z, f, mover, h, dir, pass);
 
         if (pass == 0) {
             mover.failedToRenderInFirstPass = !flag;
@@ -161,11 +92,91 @@ public class TileEntityRenderMoving extends TileEntitySpecialRenderer {
         }
     }
 
+    protected boolean renderDynamic(double x, double y, double z, float f, TileMovingClient mover, double h, ForgeDirection dir, int pass) {
+        boolean flag = false;
+        if (mover.tile != null && mover.tile.shouldRenderInPass(pass)) {
+            TileEntitySpecialRenderer specialRenderer = TileEntityRendererDispatcher.instance.getSpecialRenderer(mover.tile);
+            if (specialRenderer != null) {
+                GL11.glPushMatrix();
+                GL11.glTranslated(dir.offsetX * h, dir.offsetY * h, dir.offsetZ * h);
+                try {
+                    specialRenderer.func_147496_a(fakeWorld);
+                    specialRenderer.renderTileEntityAt(mover.tile, x, y, z, f);
+                    specialRenderer.func_147496_a(world);
+                } catch (Exception e) {
+                    FLRenderHelper.clearTessellator();
+
+                    (new RuntimeException(
+                            "Unable to render TSER " + mover.tile.getClass().getName() + " for "
+                                    + Block.blockRegistry.getNameForObject(mover.block)
+                                    + " with meta " + mover.meta + " at ("
+                                    + mover.xCoord + "," + mover.yCoord + mover.zCoord + "). Disabling Rendering."
+                            , e
+                    )).printStackTrace();
+
+                    TileMovingClient.renderErrorList.add(mover.tile.getClass());
+                    mover.error = true;
+                    mover.tile = null;
+                    mover.render = false;
+                }
+
+                flag = true;
+                GL11.glPopMatrix();
+
+            }
+        }
+        return flag;
+    }
+
+    protected boolean renderStatic(TileMovingClient mover, int pass) {
+        boolean flag = false;
+
+        Tessellator tessellator = Tessellator.instance;
+        RenderHelper.disableStandardItemLighting();
+
+        if (pass != 0) {
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        }
+
+        GL11.glEnable(GL11.GL_CULL_FACE);
+
+        if (Minecraft.isAmbientOcclusionEnabled()) {
+            GL11.glShadeModel(GL11.GL_SMOOTH);
+        } else {
+            GL11.glShadeModel(GL11.GL_FLAT);
+        }
+
+        try {
+            tessellator.setColorOpaque_F(1.0F, 1.0F, 1.0F);
+            tessellator.startDrawingQuads();
+
+            if (renderBlocks.renderBlockByRenderType(mover.block, mover.xCoord, mover.yCoord, mover.zCoord))
+                flag = true;
+
+            tessellator.draw();
+        } catch (Exception e) {
+            FLRenderHelper.clearTessellator();
+
+            (new RuntimeException(
+                    "Unable to render block " + Block.blockRegistry.getNameForObject(mover.block)
+                            + " with meta " + mover.meta + " at ("
+                            + mover.xCoord + "," + mover.yCoord + mover.zCoord + "). Disabling Rendering."
+                    , e
+            )).printStackTrace();
+
+            TileMovingClient.renderErrorList.add(mover.block.getClass());
+            mover.tile = null;
+            mover.render = false;
+            mover.error = true;
+        }
+        RenderHelper.enableStandardItemLighting();
+        return flag;
+    }
+
     public void func_147496_a(World world) {
         this.world = world;
         this.fakeWorld = FakeWorldClient.getFakeWorldWrapper(world);
         this.renderBlocks = new RenderBlocks(fakeWorld);
     }
-
-
 }
